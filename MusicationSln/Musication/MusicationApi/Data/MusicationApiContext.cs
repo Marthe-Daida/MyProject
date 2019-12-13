29e867c92bcbade0b2f5297ff3bb1663b9cfb65c
Marthe-Daida <55828800+Marthe-Daida@users.noreply.github.com>
2019-12-13 10:21:44 +0200

    update
    yes

---
+++ MusicationSln/Musication/MusicationApi/Data/MusicationApiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicationApi.Models
+{
+    public class MusicationApiContext : DbContext
+    {
+        public MusicationApiContext(DbContextOptions<MusicationApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MusicationApi.Models.User> Users { get; set; }
+    }
+}
---
+++ MusicationSln/Musication/MusicationApi/Data/SeedData.cs
@@ -0,0 +1,29 @@
+﻿using MusicationApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicationApi.Data
+   {
+    public class SeedData
+    {
+        public static void Initialize(MusicationApiContext context)
+        {
+            if (!context.Users.Any())
+            {
+                context.Users.AddRange(
+                    new Models.User
+                    {
+                        UserName = "Marthe",
+                        Email = "@gmail.com",
+                        Gender = "Female",
+                        Password = "123",
+
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
